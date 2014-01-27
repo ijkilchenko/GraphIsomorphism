4f563b3e957eae8acf90d5fca7d9b170b81f2549
ijk <ijkilchenko@gmail.com>
2014-01-26 22:11:36 -0500

    Implemented CheckEdges method and corresponding test.

--- src/Graph.java
+++ src/Graph.java
@@ -70,11 +70,11 @@ public class Graph{
         }
 
         boolean[] matched= new boolean[n];
-        int noMatch= -1;
+        int mismatched= -1;
         for (int i= 0; i < n; i++){
-            int mapSize= map.length;
+            int currentLength= map.length;
             for (int j= 0; j < n; j++){
-                if (matched[j] != true && j > noMatch){
+                if (matched[j] != true && j > mismatched){
                     boolean match= true;
                     if (tables1[i].length != tables2[j].length){
                         match= false;
@@ -90,7 +90,7 @@ public class Graph{
                             for (int k= 0; k < map.length; k++){
                                 int key= map.get(k*2);
                                 int mapped= map.get(k*2+1);
-                                int keyLevel= tables1[i].getLevel(key);;
+                                int keyLevel= tables1[i].getLevel(key);
                                 int mapLevel= tables2[j].getLevel(mapped);
                                 if (keyLevel != mapLevel){
                                     match= false;
@@ -100,27 +100,61 @@ public class Graph{
                         }
                     }
                     if (match == true){
-                        map.add(mapSize, i);
-                        map.add(mapSize, j);
+                        map.add(currentLength, i);
+                        map.add(currentLength, j);
                         matched[j]= true;
-                        noMatch= -1;
+                        mismatched= -1;
                         break;
                     }
                 }
             }
-            if (map.length == mapSize){
+            if (map.length == currentLength){
                 if (i-1 < 0){
-                    System.out.println("Can't find match for the very first node...");
+                    System.out.println("False. Graphs are non-isomorphic! ");
+                    return new Table();
                 }
-                noMatch= map.get((i-1)*2+1);
-                matched[noMatch]= false;
+                mismatched= map.get((i-1)*2+1);
+                matched[mismatched]= false;
                 map.pop();
                 i= map.length-1;
             }
         }
 
-        System.out.println("Breakpoint!");
-
+        System.out.println("True. Graphs are isomorphic! ");
         return map;
     }
+
+    public static boolean checkEdges(Graph G1, Graph G2, Table map){
+        long startTime = System.nanoTime();
+        int n= map.length;
+        for (int i= 0; i < n; i++){
+            int key= map.get(2*i);
+            int value= map.get(2*i+1);
+            if (G1.V[key].children.length != G2.V[value].children.length){
+                System.out.println("Error. Map sets correspondence between nodes with different number of children! ");
+                return false;
+            }
+            for (int j= 0; j < G1.V[key].children.length; j++){
+                int key1= G1.V[key].children[j].data;
+                int value1= map.get(2*key1+1);
+                boolean flag= false;
+                for (int k= 0; k < G2.V[value].children.length; k++){
+                    if (G2.V[value].children[k].data == value1){
+                        flag= true;
+                    }
+                }
+                if (flag == false){
+                    System.out.println("Error. An edge was not mapped! ");
+                    return false;
+                }
+
+            }
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Success! Isomorphism confirmed. Check took " + duration/Math.pow(10,9)+ "seconds");
+        return true;
+    }
+
+
 }
--- test/GraphTest.java
+++ test/GraphTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * User: ijk
@@ -90,18 +91,102 @@ public class GraphTest {
     @Test
     public void testAreIsomorphic() throws Exception {
         int n= 3;
+        int t= 10;
 
-        BitMatrix adjMatrix= AdjMatrix.makeRandom(n);
+        long totalTime= 0;
 
-        BitMatrix permMatrix= PermMatrix.makeRandom(n);
-        BitMatrix permMatrixTrans= PermMatrix.transpose(permMatrix);
-        BitMatrix adjMatrixPerm= PermMatrix.multiply(permMatrix, adjMatrix);
-        adjMatrixPerm= PermMatrix.multiply(adjMatrixPerm, permMatrixTrans);
+        /*Test isomorphic graphs*/
+        for (int i= 0; i < t; i++){
+            BitMatrix adjMatrix= AdjMatrix.makeRandom(n);
 
+            BitMatrix permMatrix= PermMatrix.makeRandom(n);
+            BitMatrix permMatrixT= PermMatrix.transpose(permMatrix);
+            BitMatrix adjMatrixPerm= PermMatrix.multiply(permMatrix, adjMatrix);
+            adjMatrixPerm= PermMatrix.multiply(adjMatrixPerm, permMatrixT);
+
+            Graph G1= new Graph(adjMatrix);
+            Graph G2= new Graph(adjMatrixPerm);
+
+            long startTime = System.nanoTime();
+            Table map= Graph.areIsomorphic(G1,G2);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            totalTime += duration;
+
+            if (map == null){
+                System.out.println("Error. Map return between isomorphic was null");
+            }
+            else{
+                if (!Graph.checkEdges(G1, G2, map)){
+
+                }
+            }
+        }
+        System.out.print("Total time is " + totalTime/Math.pow(10,9) + " seconds.");
+
+    }
+
+    @Test
+    public void testCheckEdges() throws Exception {
+        /*
+           1--4
+           |  |
+        0--3--2
+
+         */
+
+        int n= 5;
+        BitMatrix adjMatrix= new BitMatrix(n);
+        adjMatrix.setBit(0, 3, true);
+        adjMatrix.setBit(1, 3, true);
+        adjMatrix.setBit(1, 4, true);
+        adjMatrix.setBit(2, 3, true);
+        adjMatrix.setBit(2, 4, true);
+        adjMatrix.setBit(3, 0, true);
+        adjMatrix.setBit(3, 1, true);
+        adjMatrix.setBit(3, 2, true);
+        adjMatrix.setBit(4, 1, true);
+        adjMatrix.setBit(4, 2, true);
         Graph G1= new Graph(adjMatrix);
-        Graph G2= new Graph(adjMatrixPerm);
 
-        Graph.areIsomorphic(G1,G2);
+        /*
+           2--0
+           |  |
+        1--4--3
+
+         */
+
+        n= 5;
+        BitMatrix adjMatrix2= new BitMatrix(n);
+        adjMatrix2.setBit(1, 4, true);
+        adjMatrix2.setBit(2, 4, true);
+        adjMatrix2.setBit(2, 0, true);
+        adjMatrix2.setBit(3, 4, true);
+        adjMatrix2.setBit(3, 0, true);
+        adjMatrix2.setBit(4, 1, true);
+        adjMatrix2.setBit(4, 2, true);
+        adjMatrix2.setBit(4, 3, true);
+        adjMatrix2.setBit(0, 2, true);
+        adjMatrix2.setBit(0, 3, true);
+        Graph G2= new Graph(adjMatrix2);
+
+        Table map= new Table();
+
+        map.add(0, 0);
+        map.add(1, 1);
+        map.add(2, 2);
+        map.add(3, 3);
+        map.add(4, 4);
+
+        map.add(0, 1);
+        map.add(1, 2);
+        map.add(2, 3);
+        map.add(3, 4);
+        map.add(4, 0);
+
+        assertTrue(Graph.checkEdges(G1, G2, map));
+
+
 
     }
 }
